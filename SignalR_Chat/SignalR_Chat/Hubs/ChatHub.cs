376d31e1aebb615313d71a866448dd55f6674007
kieuminhdat <kieuminhdat67@gmail.com>
2024-12-23 14:12:07 +0700

    chat
    chat 2 sides

---
+++ SignalR_Chat/SignalR_Chat/Hubs/ChatHub.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalR_Chat.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task Message(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+            //this will be listen by client using javascript.
+        }
+    }
+}
